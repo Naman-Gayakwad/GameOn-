41056ec4df78d740f3a80c4cb41933bd577623f3
Naman-Gayakwad <110586020+Naman-Gayakwad@users.noreply.github.com>
2023-05-20 17:53:45 +0530

    indoor list  fetched with firebase

--- lib/indoorlist.dart
+++ lib/indoorlist.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:game_on/screens/mainpage.dart';
 import 'package:game_on/utils/next_screen.dart';
@@ -15,15 +16,7 @@ class _IndoorListState extends State<IndoorList> {
 
   bool startAnimation = false;
 
-  List<String> texts = [
-    "Chess",
-    "Carrom",
-    "Pool",
-    "Table Tennis",
-    "Yoga",
-    "Meditation",
-    "Arm Wrestling",
-  ];
+  
 
   @override
   void initState() {
@@ -38,6 +31,8 @@ class _IndoorListState extends State<IndoorList> {
 
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> _indoorlistStream =
+        FirebaseFirestore.instance.collection('indoorlist').snapshots();
     screenHeight = MediaQuery.of(context).size.height;
     screenWidth = MediaQuery.of(context).size.width;
     double baseWidth = 430;
@@ -87,70 +82,83 @@ class _IndoorListState extends State<IndoorList> {
                 const SizedBox(
                   height: 20,
                 ),
-                GestureDetector(
-                  onTap: () {
-                    nextScreen(
-                      context,
-                      MainPage(),
-                    );
-                  },
-                  child: ListView.builder(
-                    primary: false,
-                    shrinkWrap: true,
-                    itemCount: texts.length,
-                    itemBuilder: (context, index) {
-                      return item(index);
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
+                StreamBuilder<QuerySnapshot>(
+                    stream: _indoorlistStream,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('Something went wrong');
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Text("Loading");
+                      }
+
+                      return GestureDetector(
+                        onTap: () {
+                          nextScreen(
+                            context,
+                            MainPage(),
+                          );
+                        },
+                        child: ListView.builder(
+                          primary: false,
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            final indoorlistData = snapshot.data!.docs[index];
+                            return AnimatedContainer(
+                              height: 70,
+                              width: screenWidth,
+                              curve: Curves.easeInOut,
+                              duration:
+                                  Duration(milliseconds: 300 + (index * 200)),
+                              transform: Matrix4.translationValues(
+                                  startAnimation ? 0 : screenWidth, 0, 0),
+                              margin: const EdgeInsets.only(
+                                bottom: 12,
+                              ),
+                              padding: EdgeInsets.symmetric(
+                                horizontal: screenWidth / 20,
+                              ),
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFE0EAFC),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: GestureDetector(
+                                onTap: () {
+                                  print(
+                                      "Selected Indoor Sport : $indoorlistData['sportName'] ");
+                                  nextScreen(
+                                    context,
+                                    MainPage(),
+                                  );
+                                },
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      indoorlistData['sportName'],
+                                      style: const TextStyle(
+                                        fontSize: 26,
+                                        fontWeight: FontWeight.w900,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 50,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }),
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget item(int index) {
-    return AnimatedContainer(
-      height: 70,
-      width: screenWidth,
-      curve: Curves.easeInOut,
-      duration: Duration(milliseconds: 300 + (index * 200)),
-      transform:
-          Matrix4.translationValues(startAnimation ? 0 : screenWidth, 0, 0),
-      margin: const EdgeInsets.only(
-        bottom: 12,
-      ),
-      padding: EdgeInsets.symmetric(
-        horizontal: screenWidth / 20,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(0xFFE0EAFC),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: GestureDetector(
-        onTap: () {
-          print("Selected Indoor Sport : ${texts[index]} ");
-          nextScreen(
-            context,
-            MainPage(),
-          );
-        },
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "${texts[index]}",
-              style: const TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-          ],
         ),
       ),
     );
