93b93aff65822c82ccc2e8022a737cb9529952b1
Naman-Gayakwad <110586020+Naman-Gayakwad@users.noreply.github.com>
2023-05-24 10:03:39 +0530

    Product detail

---
+++ lib/ProductDetail/product_detail.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+
+class ProductDetail extends StatefulWidget {
+  final dynamic productData;
+
+  const ProductDetail({super.key, required this.productData});
+
+  @override
+  State<ProductDetail> createState() => _ProductDetailState();
+}
+
+class _ProductDetailState extends State<ProductDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        title: Text(widget.productData['productName'], style: TextStyle(color: Colors.black),),
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: PhotoView(imageProvider: NetworkImage(widget.productData['image']),
+                ),
+              ),
+              // Positioned(child: Container(
+              //   height: 50,
+              //   width: MediaQuery.of(context).size.width,
+              // ))
+            ],
+          ),
+          SizedBox(height: 20),
+          Text('\₹' + widget.productData['price'].toString(),style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.yellowAccent),),
+          Text('\₹' + widget.productData['productName'].toString(),style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+        ],
+      ),
+    );
+  }
+}
